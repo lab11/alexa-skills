bb1c7dac9a9c191483f4cc275947c6b19a823a2a
Branden Ghena <brghena@umich.edu>
2015-07-06 12:41:51 -0400

    Remove commented out UDP stuff

--- door/src/index.js
+++ door/src/index.js
@@ -50,19 +50,6 @@ Door.prototype.eventHandlers.onLaunch = function (launchRequest, session, respon
     console.log("***Door launched***");
 
     // control the door
-    /*
-    var DOOR4908_IPV6 = '2607:f018:800:10f:c298:e552:5048:d86e';
-    var DOOR4908_PORT = 4999;
-    var message = new Buffer('unlock4908');
-    var client = dgram.createSocket('udp6');
-    client.send(message, 0, message.length, DOOR4908_PORT, DOOR4908_IPV6, function(err, bytes) {
-        if (err){
-            console.log("***Error: %j***");
-            throw err;
-        }
-        client.close();
-    });
-    */
     console.log(request);
     request.post(config.accessors_address, function(error, http_resp, body) {
         if (!error && http_resp.statusCode == 200) {
@@ -85,18 +72,7 @@ Door.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, ses
 
 Door.prototype.intentHandlers = {
     // register custom intent handlers
-    /*
-    DoorIntent: function (intent, session, response) {
-        console.log("***DoorIntent***");
-        console.log("***%j***", intent);
-        var words = intent.slots.words.value;
-        response.tellWithCard(words, "Door", words);
-    },
-    HelpIntent: function (intent, session, response) {
-        console.log("***HelpIntent***");
-        response.ask("Shut up Josh!");
-    }
-    */
+    // none
 };
 
 // Create the handler that responds to the Alexa Request.
